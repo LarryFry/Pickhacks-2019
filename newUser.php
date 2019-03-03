cc96943a4bf4698213b5e863dd12a67122b29eb6
Cole Davis <coledavis6499@gmail.com>
2019-03-03 05:45:59 -0600

    Got signup working

--- newUser.php
+++ newUser.php
@@ -2,22 +2,22 @@
    include('config.php');
    session_start();
    $error = "";
-   
-   if($_SERVER["REQUEST_METHOD"] == "POST") {
-      // username and password sent from form 
-      
-      $myusername = mysqli_real_escape_string($db,$_POST['username']);
-      $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
-      $myemail = mysqli_real_escape_string($db,$_POST['email']); 
 
-      
-      $sql = "INSERT INTO users(`username`, `password`, `email`) VALUES ('$myusername','$mypassword','$myemail') ";
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+      // username and password sent from form
+
+      $myusername = mysqli_real_escape_string($db,$_POST['username']);
+      $mypassword = mysqli_real_escape_string($db,$_POST['password']);
+      $myemail = mysqli_real_escape_string($db,$_POST['email']);
+
+
+      $sql = "INSERT INTO users(`username`, `password`, `email`) VALUES ('$myusername','$mypassword','$myemail')";
       $result = mysqli_query($db,$sql);
-      
-    
-      
+
+
+
       // If result matched $myusername and $mypassword, table row must be 1 row
-		
+
       if($result) {
          header("location: login.php");
       }else {
@@ -26,50 +26,51 @@
    }
 ?>
 
-<html>
-   
-   <head>
-      <title>Login Page</title>
-      
-      <style type = "text/css">
-         body {
-            font-family:Arial, Helvetica, sans-serif;
-            font-size:14px;
-         }
-         label {
-            font-weight:bold;
-            width:100px;
-            font-size:14px;
-         }
-         .box {
-            border:#666666 solid 1px;
-         }
-      </style>
-      
-   </head>
-   
-   <body bgcolor = "#FFFFFF">
-	
-      <div align = "center">
-         <div style = "width:300px; border: solid 1px #333333; " align = "left">
-            <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Create Account</b></div>
-				
-            <div style = "margin:30px">
-               
-               <form action = "" method = "post">
-                  <label>Username  :</label><input type = "text" name = "username" class = "box"/><br /><br />
-                  <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
-                  <label>Email  :</label><input type = "email" name = "email" class = "box" /><br/><br />
-                  <input type = "submit" value = " Submit "/><br />               
-                </form>
-               
-               <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php echo $error; ?></div>
-					
-            </div>
-				
-         </div>
-			
-      </div>
 
-   </body>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <!--THIS IS A MEME -->
+    <!-- Title Bar Image-->
+    <link href="favicon.ico" rel="shortcut icon" type="image/x-icon" />
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light" rel="stylesheet">
+    <!--Stylesheet-->
+    <link rel="stylesheet" href="CSS/index.css">
+
+    <!-- Scrollmagic -->
+    <script src="3rd_Party_Libraries/ScrollMagic/ScrollMagic.min.js"></script>
+
+	  <!-- carousel library 0-->
+    <link rel="stylesheet" type="text/css" href="http://kenwheeler.github.io/slick/slick/slick-theme.css"/>
+	  <link rel="stylesheet" type="text/css" href="CSS/slick/slick/slick.css"/>
+  </head>
+
+  <body>
+  	<div id="blackwrapper_php">
+  	  <header>
+        <h1 id="Title"><a href="index.php">Just Recipes</a></h1>
+        <a id="Signup_php" href = "newUser.php">Signup</a>
+  	  </header>
+
+      <div align = "center">
+        <div style = "width:30%;" align = "left">
+          <div style = "background-color:#333333; color:#FFFFFF;"></div>
+            <div style = "margin:30px">
+               <form action = "" method = "post" autocomplete = "off">
+                 <label class="login_instr">UserName  : </label><input class="loginBox_php"btype = "text" name = "username" class = "box"/><br /><br />
+                 <label class="login_instr">Password  : </label><input class="loginBox_php" type = "password" name = "password" class = "box" /><br/><br />
+                  <label class="login_instr">Email  : </label><input class="loginBox_php" type = "email" name = "email" class = "box" /><br/><br />
+                  <input type = "submit" value = " Submit "/><br />
+                </form>
+
+               <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php echo $error; ?></div>
+            </div>
+         </div>
+      </div>
+  </div>
+  </body>
 </html>
